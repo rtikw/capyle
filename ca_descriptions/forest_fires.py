67f20c0540e53149dfdcac806f4a52e8ff314abd
Charles Jewers <cejewers1@sheffield.ac.uk>
2018-02-23 13:59:05 +0000

    Merge

--- ca_descriptions/forest_fires.py
+++ ca_descriptions/forest_fires.py
@@ -24,12 +24,18 @@ def transition_func(grid, neighbourstates, neighbourcounts):
     # create boolean arrays for the birth & survival rules
     # if 3 live neighbours and is dead -> cell born
     start_fire = (live_neighbours >= 1) & (grid == 0)
-    # if 2 or 3 live neighbours and is alive -> survives
-    survive = grid == 1
+
+    # trees on fire in last iteration burn out
+    dies_out = (live_neighbours == 0) & (grid == 1)
+
     # Set all cells to 0 (dead)
     grid[:, :] = 0
-    # Set cells to 1 where either cell is born or survives
-    grid[start_fire | survive] = 1
+    #Set array of 'fuel' using grid
+
+    # Set cells to 1 where a fire is started or if the cell is already on fire
+    grid[start_fire] = 1
+
+
     return grid
 
 
@@ -37,7 +43,7 @@ def setup(args):
     config_path = args[0]
     config = utils.load(config_path)
     # ---THE CA MUST BE RELOADED IN THE GUI IF ANY OF THE BELOW ARE CHANGED---
-    config.title = "Conway's game of life"
+    config.title = "Forest Fires"
     config.dimensions = 2
     config.states = (0, 1)
     # ------------------------------------------------------------------------
